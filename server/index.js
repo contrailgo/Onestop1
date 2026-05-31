8e03f05929cdc5501197c23d97d5c965da1bc5ef
Lille Barro <134624663+contrailgo@users.noreply.github.com>
2026-06-01 01:20:32 +0900

    CORS 설정 추가

--- server/index.js
+++ server/index.js
@@ -6,7 +6,9 @@ const userRouter = require("./routes/user");
 const app = express();
 const PORT = process.env.PORT || 3001;
  
-app.use(cors());
+app.use(cors({
+  origin: ["https://onestop1-alpha.vercel.app", "http://localhost:5173"],
+}));
 app.use(express.json());
  
 app.use("/api/reservations", reservationRouter);
